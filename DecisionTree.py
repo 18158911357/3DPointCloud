5708c232ce281d23b1ac292e8a7e90821b084ad1
zys <614490648@qq.com>
2019-06-14 14:27:24 +0800

    更新决策树算法(ID3算法)

--- DecisionTree.py
+++ DecisionTree.py
@@ -45,42 +45,10 @@ def InfoEntropy(xDataSet):
     return infoEntropy
 
 
-# 选择分裂属性
-def f(xDataSet):
-    # 特征数量，去掉最后一列的类标号
-    numFeatures = len(xDataSet[0]) - 1
-    baseInfo = InfoEntropy(xDataSet)
-    bestInfoGain = 0.0
-    bestFeature = -1
-    for i in range(numFeatures):
-        # 获取dataSet的第i个所有特征
-        featList = [x[i] for x in xDataSet]
-        # 创建set集合{}，元素不可重复
-        uniqueVals = set(featList)
-        # 经验条件熵
-        newEntropy = 0.0
-        # 计算信息增益
-        for value in uniqueVals:
-            tempDataSet = subDataSet(xDataSet, i, value)
-            prob = len(tempDataSet) / float(len(xDataSet))
-            newEntropy += prob * InfoEntropy(tempDataSet)
-        # 信息增益
-        infoGain = baseInfo - newEntropy
-        # 打印每个特征的信息增益
-        print("第%d个特征的增益为%.3f" % (i, infoGain))
-        # 计算信息增益
-        if infoGain > bestInfoGain:
-            # 更新信息增益，找到最大的信息增益
-            bestInfoGain = infoGain
-            # 记录信息增益最大的特征的索引值
-            bestFeature = i
-            # 返回信息增益最大特征的索引值
-    return bestFeature
-
-
 def subDataSet(xDataSet, axis, xValue):
     """
     计算数据子集，按照属性值的取值进行计算
+    返回的子集需要剔除划分的属性值，即xData[axis]
 
     :param xDataSet:
     :param axis:
@@ -90,8 +58,101 @@ def subDataSet(xDataSet, axis, xValue):
     resultDataSet = []
     for xData in xDataSet:
         if xData[axis] == xValue:
-            resultDataSet.append(xData)
+            tempData = xData[:axis]
+            tempData.extend(xData[axis + 1:])
+            resultDataSet.append(tempData)
     return resultDataSet
+
+
+def bestSplitFeature(xDataSet):
+    """
+    选择最佳的分裂属性，按照信息熵确定，取信息熵最高的类标号index
+
+    :param xDataSet:
+    :return:
+    """
+    # 特征数量，去掉最后一列的类标号
+    numFeatures = len(xDataSet[0]) - 1
+    baseInfo = InfoEntropy(xDataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        # 获取dataSet的第i个特征的所有取值
+        featList = [x[i] for x in xDataSet]
+        # 剔除重复取值
+        uniqueVals = set(featList)
+        # 经验条件熵
+        newEntropy = 0.0
+        # 计算信息增益
+        for value in uniqueVals:
+            tempDataSet = subDataSet(xDataSet, i, value)
+            prob = len(tempDataSet) / float(len(xDataSet))
+            newEntropy += prob * InfoEntropy(tempDataSet)
+        infoGain = baseInfo - newEntropy
+        # 打印每个特征的信息增益
+        print("第%d个特征的增益为%.3f" % (i, infoGain))
+        # 计算信息增益
+        if infoGain > bestInfoGain:
+            # 更新信息增益，找到最大的信息增益
+            bestInfoGain = infoGain
+            # 记录信息增益最大的特征的索引值
+            bestFeature = i
+    print('******************************')
+    return bestFeature
+
+
+def majorClass(xClassList):
+    """
+    计算xClassList中出现次数最多的类标签
+
+    :param xClassList: 类标签集合
+    :return: 出现次多的类标签
+    """
+    xClassCount = {}
+    # 统计xClassList中每个元素出现的次数
+    for vote in xClassList:
+        if vote not in xClassCount.keys():
+            xClassCount[vote] = 0
+        xClassCount[vote] += 1
+        # 根据字典的值降序排列
+        sortedClassCount = sorted(xClassCount.items(), key=operator.itemgetter(1), reverse=True)
+        return sortedClassCount[0][0]
+
+
+def createTree(xDataSet, xLabels, xFeatLabels):
+    """
+    创建决策树
+
+    :param xDataSet:
+    :param xLabels:
+    :param xFeatLabels:
+    :return:
+    """
+    # 取分类标签（是否放贷：yes or no）
+    classList = [x[-1] for x in xDataSet]
+    # 如果类别完全相同，则停止继续划分
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 遍历完所有特征时返回出现次数最多的类标签
+    if len(dataSet[0]) == 1:
+        return majorClass(classList)
+    # 选择最优特征
+    bestIndex = bestSplitFeature(xDataSet)
+    bestFeatLabel = xLabels[bestIndex]
+    featLabels.append(bestFeatLabel)
+    # 根据最优特征的标签生成树
+    DectionTree = {bestFeatLabel: {}}
+    # 删除已经使用的特征标签
+    del (xLabels[bestIndex])
+    # 得到训练集中所有最优特征的属性值
+    featValues = [x[bestIndex] for x in xDataSet]
+    # 去掉重复的属性值
+    uniqueVls = set(featValues)
+    # 遍历特征，创建决策树
+    for value in uniqueVls:
+        tempDataSet = subDataSet(xDataSet, bestIndex, value)
+        DectionTree[bestFeatLabel][value] = createTree(tempDataSet, xLabels, xFeatLabels)
+    return DectionTree
 
 
 def createTestData():
@@ -120,8 +181,9 @@ def createTestData():
 
 if __name__ == '__main__':
     from math import log
+    import operator
 
-    testData, testLabel = createTestData()
-    bestIndex = f(testData)
-
-    print("最优索引值：" + testLabel[bestIndex])
+    dataSet, labels = createTestData()
+    featLabels = []
+    myTree = createTree(dataSet, labels, featLabels)
+    print(myTree)
