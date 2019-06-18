9b7542c27aff37b3093feebe5a0cb47b57da6ab6
zys <614490648@qq.com>
2019-06-18 09:16:20 +0800

    更新Line2D，添加get,set

--- CommonStruct/Line2D.py
+++ CommonStruct/Line2D.py
@@ -1,5 +1,5 @@
 class Line2D:
-    def __init__(self, xa, xb, xc):
+    def __init__(self, xa=1, xb=1, xc=0):
         """
         ax + by + c = 0
 
@@ -11,3 +11,35 @@ class Line2D:
         self.__a = xa
         self.__b = xb
         self.__c = xc
+
+    @property
+    def a(self):
+        return self.__a
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def c(self):
+        return self.__c
+
+    @a.setter
+    def a(self, xa):
+        self.__a = xa
+
+    @b.setter
+    def b(self, xb):
+        self.__b = xb
+
+    @c.setter
+    def c(self, xc):
+        self.__c = xc
+
+    def __str__(self):
+        return '[%s, %s, %s]' % (self.__a, self.__b, self.__c)
+
+
+if __name__ == '__main__':
+    testLine = Line2D()
+    print(testLine)
