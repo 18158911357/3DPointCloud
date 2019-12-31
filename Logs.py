b0141decd6e7ed1ca761968b2a75e796472faeb8
ZYS_Future <614490648@qq.com>
2019-12-31 15:54:29 +0800

    更新日志代码

--- Logs.py
+++ Logs.py
@@ -3,50 +3,50 @@ import time
 Flag_Test = False  # 是否进入调试
 
 
-def getLocalDate():
+def GetLocalDate():
     """
     获取当前日期并格式化为str类型
     """
     return time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
 
-def getLocalTime():
+def GetLocalTime():
     """
     获取当前时间并格式化为str类型
     """
     return time.strftime('%X', time.localtime(time.time()))
 
 
-def getLogFileName():
+def GetLogFileName():
     """
     获取当日的log文件名，文件名为‘log-当前日期’
     """
-    return 'log/logs-' + getLocalDate() + '.txt'
+    return 'log/logs-' + GetLocalDate() + '.txt'
 
 
-def newLogFile():
+def NewLogFile():
     """
     新建log文件，文件名为‘log-当前日期’
     """
-    xLogFilePath = getLogFileName()
+    xLogFilePath = GetLogFileName()
     with open(xLogFilePath, 'w') as f:
-        print('##############################  %s  ##############################' % getLocalDate(), file=f)
+        print('##############################  %s  ##############################' % GetLocalDate(), file=f)
 
 
-def writeLogFile(xs):
+def WriteLogFile(xs):
     """
     将字符串xs写入log文件中，并加入当前时间
     """
-    xLogFileName = getLogFileName()
+    xLogFileName = GetLogFileName()
     with open(xLogFileName, 'a') as xLogFile:
-        print('%s || ' % getLocalTime() + xs, file=xLogFile)
+        print('%s || ' % GetLocalTime() + xs, file=xLogFile)
 
 
 if __name__ == '__main__':
     if Flag_Test:
         start = time.time()
-        newLogFile()
+        NewLogFile()
         for i in range(10000):
-            writeLogFile("我就是试试，试试就试试!")
+            WriteLogFile("我就是试试，试试就试试!")
         end = time.time()
         print(end - start)
